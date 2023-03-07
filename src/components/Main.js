12f66c625b9032c05609a689f773ac485382c89f
Hatim Margi <hatimmargi4@gmail.com>
2023-03-07 21:57:15 +0200

    final commit

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+export default function Navbar () {
+    return (
+            <main>
+                <h1>Fun Facts About React</h1>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                    <li>Lorem ipsum dolor sit amet m dolor sit amet consectetur adipisicing.</li>
+                </ul>
+            </main>
+    )
+
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import logo from '../imgs/logo192.png'
+export default function Navbar () {
+    return (
+   <nav>
+    <img src={logo} alt='reactlogo' />
+    <h3>project 1</h3>
+   </nav>
+    )
+
+}
